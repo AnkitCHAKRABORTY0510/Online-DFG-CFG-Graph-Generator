8573899ac8e747bd29aa2251d4abf5da52572dc4
AnkitCHAKRABORTY0510 <ankitchakraborty0510@gmail.com>
2024-10-09 22:05:11 +0530

    download files to local machine

---
+++ TestFile/main.cpp
@@ -0,0 +1,10 @@
+// main.cpp file 1
+#include <iostream>
+#include "math_operations.h"
+
+int main() {
+    int x = 10, y = 5;
+    std::cout << "Add: " << add(x, y) << std::endl;
+    std::cout << "Subtract: " << subtract(x, y) << std::endl;
+    return 0;
+}
